8778d156efe324ad9e435384f897003471e07a9e
aleontiev <alonetiev@gmail.com>
2021-12-09 14:07:53 -0500

    1.1.7

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
 from __future__ import absolute_import
 from setuptools import find_packages, setup
 
-NAME = 'dynamic-rest-client'
-DESCRIPTION = 'Python client for dynamic-rest with minimal dependencies'
-URL = 'http://github.com/AltSchool/dynamic-rest-client'
-VERSION = '0.1.7'
+NAME = "dynamic-rest-client"
+DESCRIPTION = "Python client for dynamic-rest with minimal dependencies"
+URL = "http://github.com/AltSchool/dynamic-rest-client"
+VERSION = "1.1.7"
 SCRIPTS = []
 
 setup(
     description=DESCRIPTION,
     include_package_data=True,
-    install_requires=open('install_requires.txt').readlines(),
-    long_description=open('README.rst').read(),
+    install_requires=open("install_requires.txt").readlines(),
+    long_description=open("README.rst").read(),
     name=NAME,
     packages=find_packages(),
     scripts=SCRIPTS,
     url=URL,
-    version=VERSION
+    version=VERSION,
 )
